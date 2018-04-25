323e95dc161082b2374dba946cc1b0dfa4a63279
rekols <rekols@foxmail.com>
2018-04-25 17:05:15 +0800

    powerful load file

---
+++ custom.el
@@ -0,0 +1,31 @@
+(defun add-subdirs-to-load-path (dir)
+  "Recursive add directories to 'load-path'."
+  (let ((default-directory (file-name-as-directory dir)))
+    (add-to-list 'load-path dir)
+   (normal-top-level-add-subdirs-to-load-path)))
+
+(add-subdirs-to-load-path "~/remacs/lisp/")
+(add-subdirs-to-load-path "~/remacs/plugins/")
+
+(require 'dark-theme)
+(require 'basic-tools)
+(require 'move-text)
+(require 'paredit)
+(require 'less)
+(require 'hide-comnt)
+                  
+(require 'init-auto-complete)
+(require 'init-auto-save)
+(require 'init-generic)
+(require 'init-multiterm)
+(require 'init-linum)
+(require 'init-backup)
+(require 'init-dired)
+(require 'init-indent)
+(require 'init-tabbar)
+(require 'init-yasnippet)
+(require 'init-paredit)
+(require 'init-key)
+(require 'init-ide)
+
+(provide 'custom)
