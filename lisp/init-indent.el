a2d88a252182af168bc1d802342328dc0a414fcb
rekols <rekols@foxmail.com>
2018-04-25 20:09:18 +0800

    optimize indent

--- lisp/init-indent.el
+++ lisp/init-indent.el
@@ -1,22 +1,7 @@
-;; (setq-default indent-tabs-mode t)       ;默认用Tab模式
-;; (setq default-tab-width 4)              ;设置TAB默认的宽度
-;; (setq-default c-basic-offset 4)
+(setq-default indent-tabs-mode nil)
+(setq-default c-basic-offset 4)
 
-(defun my-c-mode-common-hook ()
- ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
- (c-set-offset 'substatement-open 0)
- ;; other customizations can go here
-
- (setq c++-tab-always-indent t)
- (setq c-basic-offset 4)                  ;; Default is 2
- (setq c-indent-level 4)                  ;; Default is 2
-
- (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
- (setq tab-width 4)
- (setq indent-tabs-mode t)  ; use spaces only if nil
- )
-
-(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
-(add-hook 'c++-mode-hook 'my-c-mode-common-hook)
+(setq default-tab-width 4)
+(infer-indentation-style)
 
 (provide 'init-indent)
