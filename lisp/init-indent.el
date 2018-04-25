a96ccde16104053f9d90516a0533a638b03f559b
rekols <rekols@foxmail.com>
2018-04-25 20:31:50 +0800

    disable tab always indent

--- lisp/init-indent.el
+++ lisp/init-indent.el
@@ -1,7 +1,7 @@
 (setq-default indent-tabs-mode nil)
 (setq-default c-basic-offset 4)
+(setq-default tab-always-indent nil)
 
 (setq default-tab-width 4)
-(infer-indentation-style)
 
 (provide 'init-indent)
