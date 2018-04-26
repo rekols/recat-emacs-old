89392cbcad88bddff14a5a8219457d950fd0d0a2
rekols <rekols@foxmail.com>
2018-04-26 10:51:55 +0800

    optimize for c++ lambda function

--- lisp/init-ide.el
+++ lisp/init-ide.el
@@ -15,4 +15,18 @@
         try-expand-whole-kill              ;kill-ring里面补全
         ))
 
+;; optimize for c++ lambda function.
+(defadvice c-lineup-arglist (around my activate)
+  "Improve indentation of continued C++11 lambda function opened as argument."
+  (setq ad-return-value
+        (if (and (equal major-mode 'c++-mode)
+                 (ignore-errors
+                   (save-excursion
+                     (goto-char (c-langelem-pos langelem))
+                     ;; Detect "[...](" or "[...]{". preceded by "," or "(",
+                     ;;   and with unclosed brace.
+                     (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
+            0                           ; no additional indent
+          ad-do-it)))                   ; default behavior
+
 (provide 'init-ide)
