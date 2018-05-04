a5c90504d97169e3fe0fdb93d529017c21cde565
Unshun White <rekols@foxmail.com>
2018-05-04 11:36:37 +0800

    optimize multi-term

--- lisp/init-multiterm.el
+++ lisp/init-multiterm.el
@@ -1,7 +1,7 @@
 (require 'multi-term)
 
+(setq multi-term-program "/bin/bash")
 (setq term-eol-on-send t)                         ;输入前跳转到最后一行
-(setq multi-term-dedicated-skip-other-window-p t) ;`other-window' 不在专注窗口中经过
 (setq multi-term-scroll-show-maximum-output t)    ;最大输出时滚动
 (setq multi-term-scroll-to-bottom-on-output nil)  ;到达底部时不滚动
 (setq multi-term-dedicated-select-after-open-p t) ;打开专注终端窗口时聚焦
