90cb512c8c88e0343f9f95c91dde59482fd8c7ca
rekols <rekols@foxmail.com>
2018-05-07 11:12:45 +0800

    startup fullscreen

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -23,7 +23,7 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)                                  
-(maximize)
+(fullscreen)
 (set-face-attribute 'default nil :height 145) ;设置字体大小
 
 (provide 'init-startup)
